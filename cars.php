a522c264f1a7d7625f1ca65b2ba3eed9236de6dd
Remy-Amor <105914423@student.swin.edu.au>
2025-05-09 15:03:31 +1000

    created settings.php and cars.php and connected database to php

---
+++ cars.php
@@ -0,0 +1,35 @@
+<?php
+     require_once"settings.php";
+     $dbconn = @mysqli_connect($host,$user,$pwd,$sql_db);
+     if($dbconn) {
+          $query = "SELECT * FROM cars";
+          $result = mysqli_query($dbconn,$query);
+          if($result){
+               echo "<table>
+                    <thead>
+                    <tr>
+                    <th>Car ID</th>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>Price</th>
+                    <th>Year of Manufacture</th>
+                    </thead>";
+               while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $row['car_id'] . "</td>";
+                    echo "<td>" . $row['make'] . "</td>";
+                    echo "<td>" . $row['model'] . "</td>";
+                    echo "<td>" . $row['price'] . "</td>";
+                    echo "<td>" . $row['yom'] . "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+          }
+          else {
+               echo "<p>There are no cars to display bro </p>";
+          }
+
+          mysqli_close($dbconn);
+     }
+     else echo"<p>Unable to connect to the db</p>";
+?>
